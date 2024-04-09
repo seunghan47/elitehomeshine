0419525c5f0c4844edd38196c6d702588fb7e035
Paul Lim <paulslim47@gmail.com>
2024-04-09 15:23:28 -0400

    color css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./pages/Root";
 
 const App = () => {
   const route = createBrowserRouter([
     {
-      path: ,
-      element: <RootLayout />
-    }
-  ])
-  return <RouterProvider router={route}></RouterProvider>
+      path: "/",
+      element: <Root />,
+    },
+  ]);
+  return <RouterProvider router={route}></RouterProvider>;
 };
 
 export default App;
