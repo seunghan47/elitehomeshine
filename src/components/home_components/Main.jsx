977cea9c88086a7d3a2fcb4d0842fc2e2d09a481
Paul Lim <paulslim47@gmail.com>
2024-04-16 18:49:31 -0400

    style change

--- src/components/home_components/Main.jsx
+++ src/components/home_components/Main.jsx
@@ -9,7 +9,14 @@ const Main = () => {
       />
       <div>
         <h3>About Us</h3>
-        <p>hi</p>
+        <p>
+          At EliteHomeShine, we pride ourselves on delivering top-tier luxury home cleaning and maintenance services.
+          With meticulous attention to detail and a dedication to exceeding our clients' expectations, we ensure that
+          every corner of your home sparkles with perfection. Our highly trained team utilizes eco-friendly products and
+          state-of-the-art techniques to tailor our services to your unique needs, whether you reside in a sprawling
+          estate or a cozy apartment. Experience the epitome of cleanliness and sophistication with EliteHomeShine,
+          where every clean is an elite experience.
+        </p>
       </div>
     </div>
   );
