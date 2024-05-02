8c57c47f67b029004c0b6bfd56a9ee9e501f7fc2
Paul Lim <polim@Pauls-MacBook-Air.local>
2024-05-02 17:07:32 -0400

    hiring updates

--- frontend/src/pages/Careers.jsx
+++ frontend/src/pages/Careers.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styles from "./Careers.module.css";
+import Button from "../util/Button";
 
 const Careers = () => {
   const [formData, setFormData] = useState({
@@ -32,13 +33,53 @@ const Careers = () => {
 
   return (
     <div className={styles.careerPage}>
-      <h1>Join The Team</h1>
+      <div>
+        <h2>Join Our Team at EliteHomeShine!</h2>
+        <h3>Company and Culture</h3>
+        <p>
+          EliteHomeShine is a professional cleaning service provider dedicated to delivering the highest standards of
+          cleanliness and customer satisfaction. We believe in fostering a positive work environment where our employees
+          feel valued and motivated.
+        </p>
+        <ul>
+          <li>Paid Training</li>
+          <li>Competitive Pay</li>
+          <li>Flexible Schedules</li>
+          <li>Career Growth Opportunities</li>
+          <li>Supportive Team Atmosphere</li>
+        </ul>
+        <h3>Job Position Description</h3>
+        <p>
+          As a cleaner at EliteHomeShine, you'll play a crucial role in maintaining the cleanliness and hygiene of our
+          clients' homes.
+        </p>
+        <h4>Responsibilities</h4>
+        <ul>
+          <li>Perform professional cleaning services using EliteHomeShine's approved procedures and products</li>
+          <li>Use provided equipment efficiently and safely</li>
+        </ul>
+        <h4>Qualifications</h4>
+        <ul>
+          <li>Previous experience in professional cleaning preferred</li>
+          <li>Strong attention to detail</li>
+        </ul>
+        <p>
+          This Job Description is intended to describe the general nature and level of work performed by those assigned
+          to this position. This is not a comprehensive list of all responsibilities, duties, skills, efforts, and
+          conditions associated with this position. The Company reserves the right to modify this description in the
+          future, with or without notice to the employee. This Job Description does not create an employment contract,
+          implied or otherwise, and employment with the Company remains at will. These responsibilities are subject to
+          possible modification to reasonably accommodate individuals with disabilities.
+        </p>
+        <h3>Fill out the form below to submit an application</h3>
+      </div>
+      <h1 style={{ color: " #ee1154 " }}>Join The Team</h1>
       <p>We're looking for dedicated and experienced cleaners/maids to join our team!</p>
       <form
         onSubmit={handleSubmit}
         className={styles.applicationForm}
       >
-        <div className={styles.formGroup}>
+        <div className={styles.form_group}>
           <label htmlFor='name'>Your Name</label>
           <input
             type='text'
@@ -49,7 +90,7 @@ const Careers = () => {
             required
           />
         </div>
-        <div className={styles.formGroup}>
+        <div className={styles.form_group}>
           <label htmlFor='email'>Your Email</label>
           <input
             type='email'
@@ -60,7 +101,7 @@ const Careers = () => {
             required
           />
         </div>
-        <div className={styles.formGroup}>
+        <div className={styles.form_group}>
           <label htmlFor='phone'>Your Phone Number</label>
           <input
             type='tel'
@@ -71,18 +112,22 @@ const Careers = () => {
             required
           />
         </div>
-        <div className={styles.formGroup}>
-          <label htmlFor='experience'>Your Cleaning Experience (in years)</label>
-          <input
-            type='text'
-            id='experience'
+        <div className={styles.form_group}>
+          <label htmlFor='experience'>Years of experience</label>
+          <select
             name='experience'
-            value={formData.experience}
             onChange={handleChange}
             required
-          />
+          >
+            <option value=''>-- Please select --</option>
+            <option value='1'>1</option>
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+            <option value='4'>4</option>
+            <option value='5'>{`5 or more`}</option>
+          </select>
         </div>
-        <div className={styles.formGroup}>
+        <div className={styles.form_group}>
           <label htmlFor='message'>Additional Message (optional)</label>
           <textarea
             id='message'
@@ -91,7 +136,12 @@ const Careers = () => {
             onChange={handleChange}
           ></textarea>
         </div>
-        <button type='submit'>Submit Application</button>
+        <Button
+          className={styles.button}
+          type='submit'
+        >
+          Submit{" "}
+        </Button>
       </form>
     </div>
   );
