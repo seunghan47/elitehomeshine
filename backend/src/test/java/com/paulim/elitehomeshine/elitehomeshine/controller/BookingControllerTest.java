a8d2de6ba9859277875a6a5923aba908c755bf17
Paul Lim <paulslim47@gmail.com>
2025-02-04 23:41:58 -0500

    starting back up

---
+++ backend/src/test/java/com/paulim/elitehomeshine/elitehomeshine/controller/BookingControllerTest.java
@@ -0,0 +1,22 @@
+package com.paulim.elitehomeshine.elitehomeshine.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookingControllerTest {
+
+    // should/when for best practice.
+    @Test
+    void test() {
+        assertTrue(true);
+
+        //given
+
+
+        //when
+
+
+        //then
+    }
+}
